0ddb590a641710835a3f76e4cb16ae7004bb6a37
sumeet <9582776569sk@gmail.com>
2026-04-23 18:32:49 +0530

    updating the variable file in terraform for linux

--- Variable_Ec2_linux_1/main.tf
+++ Variable_Ec2_linux_1/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "mysample" {
-    ami = "i-004b72bce378138fc"
+    ami = var.ami
     instance_type = "t2.micro"
-    vpc_security_group_ids = aws_security_group.my_sg.id
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     tags = {
         Name = "mysimple-instance"
     }
@@ -20,19 +20,19 @@ resource "aws_security_group" "my_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.cidr]
     }
 
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [var.cidr]
     }
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [var.cidr]
    }
 }
---
+++ Variable_Ec2_linux_1/variable.tf
@@ -0,0 +1,8 @@
+variable "cidr" {
+  default = "0.0.0.0/0"
+
+}
+
+variable "ami" {
+  default = "ami-0de6934e87badb694"
+}
